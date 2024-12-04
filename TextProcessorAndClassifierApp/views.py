74f682ca50f1b3a64b0914da9e4aa61b1d4b9a2b
Huseyin TUNGA <tunga701@gmail.com>
2024-12-04 17:14:18 +0300

    Sayfalar projeye eklendi, "urls.py" düzenlemeleri yapıldı.

--- TextProcessorAndClassifierApp/views.py
+++ TextProcessorAndClassifierApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+def HomePage(request):
+    return render(request, 'Home.html')
+
+def UploadPage(request):
+    return render(request, 'UploadCsv.html')
+
+def process_columns(request):
+    return render(request, 'ProcessColumns.html')
+
+def process_text(request):
+    return render (request, 'ProcessTexts.html')
+
+def predict_class(request):
+    return render(request, 'PredictClasses.html')
+
+def AboutMePage(request):
+    return render(request, 'AboutMe.html')
