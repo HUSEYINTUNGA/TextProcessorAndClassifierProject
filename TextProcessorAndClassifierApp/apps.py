8534ba2eedf735b8035b7b17d60ea930ef8390c2
Huseyin TUNGA <tunga701@gmail.com>
2024-12-04 16:10:21 +0300

    TextProcessorAndClassifier projesi başarıyla başlatıldı. "settings.py" ayarlamaları yapıldı.

---
+++ TextProcessorAndClassifierApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TextprocessorandclassifierappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TextProcessorAndClassifierApp'
