8a118d5123d16beb893d702dc73e531aafce53f8
Huseyin TUNGA <tunga701@gmail.com>
2024-12-04 16:19:27 +0300

    Projeye database modelleri eklendi, migrate edildi.

--- TextProcessorAndClassifierApp/models.py
+++ TextProcessorAndClassifierApp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class TurkishTexts(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField()
+    category = models.IntegerField()
+    
+    def __str__(self):
+        return f'Text ID: {self.id}, Category: {self.category}'
+    
+class UploadedTurkishTexts(models.Model):
+    text=models.TextField()
+    category=models.IntegerField()
+    
+    def __str__(self):
+        return f'Uploaded Text: {self.text}, Category: {self.category}'
+    
+class EnglishTexts(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.TextField()
+    category = models.IntegerField()
+
+    def __str__(self):
+        return f'Text ID: {self.id}, Category: {self.category}'
+
+class UploadedEnglishTexts(models.Model):
+    text=models.TextField()
+    category=models.IntegerField()
+    
+    def __str__(self):
+        return f'Uploaded Text: {self.text}, Category: {self.category}'
